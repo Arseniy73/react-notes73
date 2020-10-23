db8253f90c4e10f7918ed3200b2c409f6a24a934
arseniy_korzhov <transporter777@mail.ru>
2020-10-23 11:11:11 +0400

    modified

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => (
+    <div className="jumbotron">
+        <div className="container">
+            <h1 className="display-4">Приложение <strong>Заметки</strong> на React</h1>
+            <p className="lead">Написано с душой, но без мозгов</p>
+        </div>
+    </div> 
+)
